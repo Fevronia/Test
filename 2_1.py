1ed5fe5e61fb04ba22002b6851060b600ac33200
Fevronia <nasta.lutchenko@gmail.com>
2017-02-26 03:38:13 +0200

    Add files via upload

---
+++ 2_1.py
@@ -0,0 +1,9 @@
+def func ():
+    number = int (input( 'Type a number'))
+    return  (number)%2 == 0
+if func():
+    print ("Number you typed is a even number")
+else:
+    print ("Number you typed is a odd number")
+
+
---
+++ 2_2.py
@@ -0,0 +1,6 @@
+def func (n):
+    for i in range(n,-1,-1):
+        print(i)
+
+n = int( input( "n = "))
+func(n)
